55c4326770ec998c196072c3cce7dd3e2daba5e4
Paperspace Guest <paperspaceguest@Paperspaces-MacBook-Pro.local>
2019-06-13 17:57:08 -0400

    Run triggers

--- mnist.py
+++ mnist.py
@@ -12,7 +12,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 """Convolutional Neural Network Estimator for MNIST, built with tf.layers."""
-#k
+#kk
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
